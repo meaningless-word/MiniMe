8df962dfe7237a19de6122c5d037b143f364f549
meaningless-word <grisly@mail.ru>
2024-02-12 00:38:33 +0700

    message api management with check

--- app/urls.py
+++ app/urls.py
@@ -12,9 +12,4 @@ router.register(r'message', views.MessageViewSet)
 urlpatterns = [
     path('', views.ChatListView.as_view(), name='home'),
     path('api/', include(router.urls), name='api'),
-    path('chat/<int:pk>/', views.get_chat),
-    path('chats/', views.get_chats),
-    path('create_chat/', views.create_chat),
-    path('edit_chat/<int:pk>/', views.edit_chat),
-    path('delete_chat/<int:pk>/', views.delete_chat),
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (ListView, )
 from django.http import HttpResponse
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 import json
 
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+
 from . import models
 from . import serializers
 
@@ -18,38 +20,6 @@ class ChatListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         queryset = super().get_queryset().filter(members__in=[self.request.user])
         return queryset
-
-
-def get_chat(_, pk):
-    chat = models.Chat.objects.get(pk=pk)
-    return HttpResponse(content=chat, status=200)
-
-
-def get_chats(_):
-    chats = models.Chat.objects.all()
-    return HttpResponse(content=chats, status=200)
-
-
-def create_chat(request):
-    body = json.loads(request.body.decode('utf-8'))
-    chat = models.Chat.objects.create(
-        title=body['title'],
-    )
-    return HttpResponse(content=chat, status=201)
-
-
-def edit_chat(request, pk):
-    body = json.loads(request.body.decode('utf-8'))
-    chat = models.Chat.objects.get(pk=pk)
-    for attr, value in body.items():
-        setattr(chat, attr, value)
-    chat.save()
-    return HttpResponse(content=chat, status=200)
-
-
-def delete_chat(_, pk):
-    models.Chat.objects.get(pk=pk).delete()
-    return HttpResponse(cstatus=204)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -73,7 +43,27 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def list(self, request):
+        chat_id = self.request.query_params.get('chat_id', None)
+        if chat_id and models.Chat.objects.filter(pk=chat_id).exists():
+            if models.Chat.objects.get(pk=chat_id).members.contains(self.request.user) or self.request.user.is_superuser:
+                data = self.serializer_class(self.queryset.filter(chat__id=chat_id), many=True).data
+                return Response(data)
 
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
+    def retrieve(self, request, pk=None):
+        chat_id = pk
+        if chat_id and models.Chat.objects.filter(pk=chat_id).exists():
+            if models.Chat.objects.get(pk=chat_id).members.contains(request.user) or request.user.is_superuser:
+                data = self.serializer_class(self.queryset.filter(chat__id=chat_id), many=True).data
+                return Response(data)
 
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.deleted = True
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
